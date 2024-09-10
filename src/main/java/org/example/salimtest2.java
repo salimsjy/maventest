b0d16e17f2a035994f3f0c52164bb88e038f3126
salim.sanjaya <salimsjaya@gmail.com>
2024-09-10 19:55:11 +0700

    praticalassesments3

--- src/main/java/org/example/salimtest2.java
+++ src/main/java/org/example/salimtest2.java
@@ -27,56 +27,71 @@ class Vehicle {
 }
 
 class Car extends Vehicle {
-    public int numOfDoors;
+    public int numOfSeats;
 
-    public Car(String brand, String model, int year, int numOfDoors) {
+    public Car(String brand, String model, int year, int numberOfSeats) {
         super(brand, model, year);
-        this.numOfDoors = numOfDoors;
+        this.numOfSeats = numberOfSeats;
     }
     @Override
     public String vehicleSound() {
-        return "Ngeng Ngenggg";
+        return "Whooooosssshhh";
     }
     public void displayCarInfo() {
         displayInfo();
-        System.out.println("Number of Doors: " + numOfDoors);
+        System.out.println("Number of Seat: " + numOfSeats);
     }
 }
 class Motorcycle extends Vehicle {
-    public String hasSidecar;
-    public Motorcycle(String brand, String model, int year, String hasSidecar) {
+    public String hasBox;
+    public Motorcycle(String brand, String model, int year, String hasBoxParam) {
         super(brand, model, year);
-        this.hasSidecar = hasSidecar;
+        this.hasBox = hasBoxParam;
     }
 
     @Override
     public String vehicleSound() {
-        return "Broom broom";
+        return "Ngeeeeennngggg";
     }
     public void displayMotorcycleInfo() {
         displayInfo();
-        System.out.println("Has Sidecar: " + hasSidecar);
+        System.out.println("Has Box: " + hasBox);
+    }
+}
+
+class InvalidYearException extends Exception
+{
+
+    // Constructor that accepts a message
+    public InvalidYearException(String message)
+    {
+        super(message);
     }
 }
 
 public class salimtest2 {
     public static void main(String[] args) {
-        Car car = new Car("Honda", "Jazz", 2020, 4);
-        System.out.println("Description of Car:");
-        car.displayCarInfo();
-        System.out.println("Car Sound: " + car.vehicleSound());
+        try {
+            Car car = new Car("Honda", "Jazz", 2020, 7);
+            System.out.println("Description of Car:");
+            car.displayCarInfo();
+            System.out.println("Car Sound: " + car.vehicleSound());
 
-        Motorcycle motorcycle = new Motorcycle("Harley-Davidson", "Moge", 2023, "Yes");
-        System.out.println("\nDescription of Motorcycle :");
-        motorcycle.displayMotorcycleInfo();
-        System.out.println("Motorcycle Sound: " + motorcycle.vehicleSound());
+            Motorcycle motorcycle = new Motorcycle("Honda", "Virza", 2022, "Yes");
+            System.out.println("\nDescription of Motorcycle :");
+            motorcycle.displayMotorcycleInfo();
+            System.out.println("Motorcycle Sound: " + motorcycle.vehicleSound());
 
-        try{
-            int [] arrcar  = new int [2018];
-            int yearcar =  arrcar[2019];
 
-        }catch(ArrayIndexOutOfBoundsException e){
-            System.out.println("Vehicle was not created because year of vehicle is "+e.getMessage());
+            int yearcar =  car.year;
+            // car year validation
+            if (yearcar < 2024) {
+                throw new InvalidYearException("2024");
+            }
+        }catch(InvalidYearException e){
+            System.out.println("Car is not brand new, because year of car is bellow "+e.getMessage());
+        }finally {
+            System.out.println("Car and Motorcycle was created successfully");
         }
     }
 }
